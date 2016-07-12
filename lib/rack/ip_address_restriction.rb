4562db40f9a426cff4fb72a34b8f71d94cee5546
koshigoe <koshigoeb@gmail.com>
2016-07-12 13:31:53 +0900

    $ bundle gem rack-ip_address_restriction

---
+++ lib/rack/ip_address_restriction.rb
@@ -0,0 +1,7 @@
+require "rack/ip_address_restriction/version"
+
+module Rack
+  module IpAddressRestriction
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/ip_address_restriction/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module IpAddressRestriction
+    VERSION = "0.1.0"
+  end
+end
