0a9e31d3e466bf52521db5114b16f410a0900281
koshigoe <koshigoeb@gmail.com>
2016-07-12 22:43:48 +0900

    Use shared examples for Rack::Access in spec of Rack::IpAddressRestriction.

--- spec/rack/ip_address_restriction_spec.rb
+++ spec/rack/ip_address_restriction_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
+require_relative 'shared_examples_for_rack_access'
 
 describe Rack::IpAddressRestriction do
   it 'has a version number' do
     expect(Rack::IpAddressRestriction::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  it_behaves_like 'Rack::Access'
 end
